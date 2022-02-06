7c072a814460ea858f7c845ed37e9c14dcfe8305
tysreddy <reddythumuyeshwanth.neo@gmail.com>
2022-02-06 14:38:41 +0530

    divison part

---
+++ division.java
@@ -0,0 +1,6 @@
+  // performs division between numbers
+      case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;
+		
