fc62190f51aefa5e795ce7bb380e22ff48be5885
tyreddy <99129315+tyreddy@users.noreply.github.com>
2022-02-07 13:29:15 +0530

    added program for  division

--- addition.java
+++ addition.java
@@ -27,6 +27,11 @@ class Main {
         result = number1 + number2;
         System.out.println(number1 + " + " + number2 + " = " + result);
         break;
+        // performs division between numbers
+      case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;
 
       default:
         System.out.println("Invalid operator!");
